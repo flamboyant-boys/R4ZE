309150929b753a85617a02a353a1832b605d1288
Mocca101 NB <49754596+Mocca101@users.noreply.github.com>
2020-07-03 16:18:30 +0200

    Added Destructible Interface

---
+++ Assets/Scripts/Interfaces/IDestrucible.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace R4ZE.ModularBlock
+{
+    public interface IDestrucible
+    {
+        float GetHealth();
+        float SetHealth();
+
+
+        void Damage(float damageAmount);
+        void Heal(float healingAmount);
+
+
+        void OnDestroy();
+    }
+}
+
