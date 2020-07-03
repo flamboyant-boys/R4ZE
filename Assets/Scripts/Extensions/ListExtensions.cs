301c50c4299d306d62cf3f9b1f077cdd25ead317
Mario Kerndler <mariokerndler@gmail.com>
2020-07-03 04:02:08 +0200

    Create UnitTest Environment + Deleting Example Assets + ListExtensions
    - Deleted Example Assets.
    - Created UnitTest Environemt. (Tested how it all works, with a few examples - PlayMode/EditMode)
    - Created a library for ListExtensions (A few new methods that extends the generic list)

---
+++ Assets/Scripts/Extensions/ListExtensions.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace R4ZE.Extensions
+{
+    /// <summary>
+    /// Library for List Extensions
+    /// </summary>
+    public static class ListExtensions
+    {
+        /// <summary>
+        /// Adds an entry to the list, if the entry does not exist.
+        /// </summary>
+        public static void AddIfNotExists<T>(this IList<T> list, T value)
+        {
+            checkListIsNull(list);
+            if(!list.Contains(value))
+            {
+                list.Add(value);
+            }
+        }
+
+        /// <summary>
+        /// Updates an entry, if the entry exists.
+        /// </summary>
+        public static void UpdateValue<T>(this IList<T> list, T value, T newValue)
+        {
+            checkListAndValueIsNull(list, value);
+            checkValueIsNull(newValue);
+
+            if(list.Contains(value))
+            {
+                var index = list.IndexOf(value);
+                list[index] = newValue;
+            }
+        }
+
+        /// <summary>
+        /// Deletes an entry, if the entry exists.
+        /// </summary>
+        public static void DeleteIfExists<T>(this IList<T> list, T value)
+        {
+            checkListAndValueIsNull(list, value);
+
+            if (list.Contains(value))
+            {
+                list.Remove(value);
+            }
+        }
+
+        /// <summary>
+        /// Checks if the values of the list are null.
+        /// </summary>
+        public static bool AreValuesNull<T>(this IList<T> list)
+        {
+            checkListIsNull(list);
+            return list.All(x => x == null);
+        }
+
+        /// <summary>
+        /// Checks if the list is null or empty
+        /// </summary>
+        /// <returns>True if the list is null or empty, false if otherwise.</returns>
+        public static bool IsNullOrEmpty<T>(this IList<T> list)
+        {
+            return list == null || list.Count <= 0;
+        }
+
+        #region Private Methods
+        private static void checkListAndValueIsNull<T>(this IList<T> list, T value)
+        {
+            checkListIsNull(list);
+            checkValueIsNull(value);
+        }
+
+        private static void checkValueIsNull<T>(T value)
+        {
+            if (value == null) throw new ArgumentNullException(nameof(value));
+        }
+
+        private static void checkListIsNull<T>(this IList<T> list)
+        {
+            if (list == null) throw new ArgumentNullException(nameof(list));
+        }
+        #endregion
+    }
+}
+
---
+++ Assets/Tests/EditMode/ListExtensionTests.cs
@@ -0,0 +1,103 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using R4ZE.Extensions;
+using System;
+using System.Collections.Generic;
+
+namespace R4ZE.Tests
+{
+    public class ListExtensionTests
+    {
+        [Test]
+        public void TestAddIfNotExists_AddNewEntryToList_ListWithNewEntry()
+        {
+            List<int> testList = new List<int>() { 1, 2, 3 };
+            testList.AddIfNotExists(4);
+
+            Assert.AreEqual(4, testList.Count);
+            Assert.AreEqual(new List<int>() { 1, 2, 3, 4 }, testList);
+        }
+
+        [Test]
+        public void TestAddIfNotExists_AddExistingEntryToList_ListWithoutNewEntry()
+        {
+            List<int> testList = new List<int>() { 1, 2, 3};
+            testList.AddIfNotExists(3);
+
+            Assert.AreEqual(3, testList.Count);
+            Assert.AreEqual(new List<int>() { 1, 2, 3 }, testList);
+        }
+
+        [Test]
+        public void TestUpdateValue_UpdateExistingEntry_UpdatedList()
+        {
+            List<int> testList = new List<int>() { 1, 2, 3 };
+            testList.UpdateValue(2, 4);
+
+            Assert.AreEqual(3, testList.Count);
+            Assert.AreEqual(new List<int>() { 1, 4, 3 }, testList);
+        }
+
+        [Test]
+        public void TestDeleteIfExists_DeleteExistingEntry_ShortenedList()
+        {
+            List<int> testList = new List<int>() { 1, 2, 3 };
+            testList.DeleteIfExists(3);
+
+            Assert.AreEqual(2, testList.Count);
+            Assert.AreEqual(new List<int>() { 1, 2 }, testList);
+        }
+
+        [Test]
+        public void TestDeleteIfExists_DeleteNonExistingEntry_OriginalList()
+        {
+            List<int> testList = new List<int>() { 1, 2, 3 };
+            testList.DeleteIfExists(4);
+
+            Assert.AreEqual(3, testList.Count);
+            Assert.AreEqual(new List<int>() { 1, 2, 3 }, testList);
+        }
+
+        [Test]
+        public void TestAreValuesEmpty_CheckEmptyList_True()
+        {
+            List<string> testList = new List<string>() { null, null, null };
+            Assert.IsTrue(testList.AreValuesNull());
+        }
+
+        [Test]
+        public void TestAreValuesEmpty_CheckListWithNullEntries_False()
+        {
+            List<string> testList = new List<string>() { null, "b", null };
+            Assert.IsFalse(testList.AreValuesNull());
+        }
+
+        [Test]
+        public void TestAreValuesEmpty_CheckFilledList_False()
+        {
+            List<string> testList = new List<string>() { "a", "b", "c" };
+            Assert.IsFalse(testList.AreValuesNull());
+        }
+
+        [Test]
+        public void TestIsNullOrEmptz_EmptyList_True()
+        {
+            List<string> testList = new List<string>();
+            Assert.IsTrue(testList.IsNullOrEmpty());
+        }
+
+        [Test]
+        public void TestIsNullOrEmptz_NullList_True()
+        {
+            List<string> testList = null;
+            Assert.IsTrue(testList.IsNullOrEmpty());
+        }
+
+        [Test]
+        public void TestIsNullOrEmptz_FilledList_False()
+        {
+            List<int> testList = new List<int>(){ 1, 2, 3};
+            Assert.IsFalse(testList.IsNullOrEmpty());
+        }
+    }
+}
