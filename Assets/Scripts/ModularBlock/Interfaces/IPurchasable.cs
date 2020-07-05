51890d3b03723b1a57c57a508e34d1a3820fd2b0
Mocca101 NB <49754596+Mocca101@users.noreply.github.com>
2020-07-05 15:42:32 +0200

    Purchasable Interface Added

---
+++ Assets/Scripts/ModularBlock/Interfaces/IPurchasable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace R4ZE.ModularBlock
+{
+    public interface IPurchasable
+    {
+        int GetPrice();
+
+    }
+}
