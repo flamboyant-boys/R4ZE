c4420bbb185861679e00554547544daf6a7c18ed
Mocca101 NB <49754596+Mocca101@users.noreply.github.com>
2020-07-05 15:41:27 +0200

    Destructibles Interface Implemented

---
+++ Assets/Scripts/ModularBlock/Interfaces/IDestrucible.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using R4ZE.Utilities.Events;
+
+namespace R4ZE.ModularBlock.Interfaces
+{
+    public interface IDestructible
+    {
+        float Health { get; set; }
+
+
+        void Damage(float damageAmount);
+        void Heal(float healingAmount);
+
+
+        IDestructibleUnityEvent DestructionEvent { get; }
+
+        void OnDestruction();
+    }
+}
+
---
+++ Assets/Scripts/Utilities/Events/UnityEvents/IDestructibleUnityEvent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+using R4ZE.ModularBlock.Interfaces;
+
+namespace R4ZE.Utilities.Events
+{
+    public class IDestructibleUnityEvent : UnityEvent<IDestructible>
+    { }
+}
+
+
